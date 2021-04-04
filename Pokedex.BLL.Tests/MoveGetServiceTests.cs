26c3ad3412185cc8a649230defed7ab3af724f15
valvenya <chenghiskhan2000@gmail.com>
2021-04-04 06:30:11 +0300

    added Business Logic Layer tests

---
+++ Pokedex.BLL.Tests/MoveGetServiceTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoFixture;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using Pokedex.BLL.Contracts;
+using Pokedex.BLL.Implementations;
+using Pokedex.DataAccess.Contracts;
+using Pokedex.Domain;
+using Pokedex.Domain.Contracts;
+
+namespace Pokedex.BLL.Tests
+{
+    public class MoveGetServiceTests
+    {
+        [Test]
+        public async Task ValidateAsync_MovesExist_DoesNothing()
+        {
+            // Arrange
+            var fixture = new Fixture();
+            var movesContainer = new Mock<IMovesContainer>();
+            var moveIds = fixture.CreateMany<int>().ToList();
+            movesContainer.Setup(m => m.MoveIds).Returns(moveIds);
+            var moveDataAccess = new Mock<IMoveDataAccess>();
+            var moves = moveIds.Select(id => new Domain.Move() {Id = id}).ToList();
+            fixture.AddManyTo(moves);
+            moveDataAccess.Setup(x => x.GetAsync()).ReturnsAsync(moves);
+            var moveGetService = new MoveGetService(moveDataAccess.Object);
+            
+            // Act
+            var action = new Func<Task>(() => moveGetService.ValidateAsync(movesContainer.Object));
+            
+            // Assert
+            await action.Should().NotThrowAsync<Exception>();
+        }
+
+        [Test]
+        public async Task ValidateAsync_MovesNotExist_ThrowsError()
+        {
+            // Arrange
+            var fixture = new Fixture();
+            var movesContainer = new Mock<IMovesContainer>();
+            var moveIds = fixture.CreateMany<int>().ToList();
+            movesContainer.Setup(m => m.MoveIds).Returns(moveIds);
+            var moveDataAccess = new Mock<IMoveDataAccess>();
+            moveDataAccess.Setup(x => x.GetAsync()).ReturnsAsync(new List<Move>());
+            var moveGetService = new MoveGetService(moveDataAccess.Object);
+            
+            // Act
+            var action = new Func<Task>(() => moveGetService.ValidateAsync(movesContainer.Object));
+            
+            // Assert
+            await action.Should().ThrowAsync<InvalidOperationException>("Moves not found");
+        }
+
+        [Test]
+        public async Task GetAsync_ReturnsMove()
+        {
+            // Arrange
+            var fixture = new Fixture();
+            var moveIdentity = new Mock<IMoveIdentity>();
+            var id = fixture.Create<int>();
+            moveIdentity.Setup(x => x.Id).Returns(id);
+            var expected = new Move() { Id = id };
+            var moveDataAccess = new Mock<IMoveDataAccess>();
+            moveDataAccess.Setup(x => x.GetAsync(moveIdentity.Object)).ReturnsAsync(expected);
+            var moveGetService = new MoveGetService(moveDataAccess.Object);
+            
+            // Act
+            var result = await moveGetService.GetAsync(moveIdentity.Object);
+            
+            // Assert
+            result.Should().Be(expected);
+        }
+    }
+}
---
+++ Pokedex.BLL.Tests/SpeciesCreateServiceTests.cs
@@ -0,0 +1,44 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using AutoFixture;
+using FluentAssertions;
+using Moq;
+using NUnit.Framework;
+using Pokedex.BLL.Contracts;
+using Pokedex.BLL.Implementations;
+using Pokedex.DataAccess.Contracts;
+using Pokedex.Domain;
+using Pokedex.Domain.Models;
+
+namespace Pokedex.BLL.Tests
+{
+    public class SpeciesCreateServiceTests
+    {
+        [Test]
+        public async Task InsertAsync_ValidationSuccess_CreatesSpecies()
+        {
+            // Arrange
+            var fixture = new Fixture();
+            var stats = fixture.CreateMany<int>(6).ToList();
+            var species = fixture
+                .Build<SpeciesUpdateModel>()
+                .With(x => x.BaseStats, stats)
+                .Create();
+            var expected = new Species();
+
+            var moveGetService = new Mock<IMoveGetService>();
+            moveGetService.Setup(x => x.ValidateAsync(species));
+
+            var speciesDataAccess = new Mock<ISpeciesDataAccess>();
+            speciesDataAccess.Setup(x => x.InsertAsync(species)).ReturnsAsync(expected);
+
+            var speciesCreateService = new SpeciesCreateService(speciesDataAccess.Object, moveGetService.Object);
+            
+            // Act
+            var result = await speciesCreateService.InsertAsync(species);
+            
+            // Assert
+            result.Should().Be(expected);
+        }
+    }
+}
